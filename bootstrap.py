baeaec2ff2777b00999c63df9566965ed5757bb1
Yann Hodique <yann.hodique@gmail.com>
2010-08-25 00:07:38 +0200

    add Workflow and Parameter models

--- bootstrap.py
+++ bootstrap.py
@@ -1,7 +1,32 @@
 # Run this in admin console to populate the datastore
 
-from vco.models import Plugin
+from vco.models import Plugin, Workflow, Parameter
+from uuid import uuid1 as _uuid
 
+from google.appengine.ext import db
+
+# Cleanup everything
+def _cleanup(kind):
+    db.delete(kind.all())
+
+_cleanup(Plugin)
+_cleanup(Parameter)
+_cleanup(Workflow)
+
+# Create plug-ins
 p = Plugin(name="dummy", version="0.1",
            description="Dummy plug-in", display="Dummy")
 p.put()
+
+# Create workflows
+i = Parameter(name="in", type="string")
+i.put()
+o = Parameter(name="out", type="string")
+o.put()
+wf = Workflow(id=str(_uuid()),
+              name="Dummy workflow",
+              description="this workflow just does nothing",
+              input = [i.key()],
+              output = [o.key()],
+              attributes=[])
+wf.put()
--- vco/models.py
+++ vco/models.py
@@ -7,3 +7,16 @@ class Plugin(db.Model):
     version = db.StringProperty()
     description = db.StringProperty()
     display = db.StringProperty()
+
+class Parameter(db.Model):
+    name = db.StringProperty()
+    type = db.StringProperty()
+
+class Workflow(db.Model):
+    id = db.StringProperty()
+    name = db.StringProperty()
+    description = db.StringProperty()
+
+    input = db.ListProperty(db.Key)
+    output = db.ListProperty(db.Key)
+    attributes = db.ListProperty(db.Key)
