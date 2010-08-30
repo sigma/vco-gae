464f2bb8a779ed8ec5dcbd8d25258ee9b2c7b300
Yann Hodique <yann.hodique@gmail.com>
2010-08-30 08:03:08 +0200

    add profiling helper

--- vco-server.py
+++ vco-server.py
@@ -261,9 +261,27 @@ class VcoService(SOAPApplication):
 application = WSGIApplication()
 application['webservice'] = VcoService()
 
-def main():
+def real_main():
     logging.getLogger().setLevel(logging.DEBUG)
     run_wsgi_app(application)
+
+def profile_main():
+    # This is the main function for profiling
+    # We've renamed our original main() above to real_main()
+    import cProfile, pstats, StringIO
+    prof = cProfile.Profile()
+    prof = prof.runctx("real_main()", globals(), locals())
+    stream = StringIO.StringIO()
+    stats = pstats.Stats(prof, stream=stream)
+    stats.sort_stats("time")  # Or cumulative
+    stats.print_stats(80)  # 80 = how many to print
+    # The rest is optional.
+    stats.print_callees()
+    stats.print_callers()
+    # output to logs
+    logging.info("Profile data:\n%s", stream.getvalue())
+
+main = real_main
 
 if __name__ == "__main__":
     main()
