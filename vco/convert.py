44c6f1e0a22cce9b124a0e5e3b2a04234ef45e43
Yann Hodique <yann.hodique@gmail.com>
2010-10-24 16:02:53 +0200

    introduce clear separation between what's stored in datastore, what's kept in memcache, and what's emitted through SOAP

--- vco/convert.py
+++ vco/convert.py
@@ -2,85 +2,137 @@ import logging
 from types import *
 from google.appengine.ext import db
 
-def convertPlugin(plug, target=None):
-    if target is None:
-        target = ModuleInfo()
-    target._moduleName = plug.name
-    target._moduleVersion = plug.version
-    target._moduleDisplayName = plug.display
-    target._moduleDescription = plug.description
-    return target
+class PluginConverter(object):
 
-def convertParameter(param):
-    param = db.get(param)
-    p = WorkflowParameter()
-    p._name = param.name
-    p._type = param.type
-    return p
+    @staticmethod
+    def toJSON(plug):
+        json = {'name': plug.name,
+                'version': plug.version,
+                'display': plug.display,
+                'description': plug.description}
+        return json
 
-def convertParameters(params):
-    ps = ArrayOfWorkflowParameter()
-    ps._item = [convertParameter(p)
-                for p in params]
-    return ps
+    @staticmethod
+    def toSOAP(json):
+        soap = ModuleInfo()
+        soap._moduleName = json['name']
+        soap._moduleVersion = json['version']
+        soap._moduleDisplayName = json['display']
+        soap._moduleDescription = json['description']
+        return soap
 
-def convertWorkflow(wf, target=None):
-    if target is None:
-        target = Workflow()
-    target._id = wf.id
-    target._name = wf.name
-    target._description = wf.description
-    target._inParameters = convertParameters(wf.input)
-    target._outParameters = convertParameters(wf.output)
-    target._attributes = convertParameters(wf.attributes)
-    return target
+class ParameterConverter(object):
 
-def convertWorkflowToken(tok, target=None):
-    if target is None:
-        target = WorkflowToken()
-    target._id = tok.id
-    target._title = tok.title
-    target._workflowId = tok.wf.id
-    target._currentItemName = tok.cur_name
-    target._currentItemState = tok.cur_state
-    target._globalState = tok.state
-    target._startDate = "%s" % (tok.start)
-    target._endDate = "%s" % (tok.end)
-    target._xmlContent = tok.xml
-    return target
+    @staticmethod
+    def toJSON(param):
+        param = db.get(param)
+        json = {'name': param.name,
+                'type': param.type}
+        return json
 
-def convertWorkflowTokenResult(tok):
-    res = []
-    outputs = tok.wf.output
-    results = tok.getResults()
-    for out, idx in zip(outputs, range(len(outputs))):
-        attr = WorkflowTokenAttribute()
-        out = db.get(out)
-        attr._name = out.name
-        attr._type = out.type
-        attr._value = results.get(attr._name, "")
-        res.append(attr)
-    return res
+    @staticmethod
+    def toSOAP(json):
+        soap = WorkflowParameter()
+        soap._name = json['name']
+        soap._type = json['type']
+        return soap
 
-def convertPluginObject(obj, target=None):
-    if target is None:
-        target = FinderResult()
-    target._type = "%s:%s" % (obj.plugin.name, obj.type)
-    target._id = obj.obj_id
-    target._dunesUri = "dunes://service.dunes.ch/CustomSDKObject?id='%s'&dunesName='%s'" % (target._id, target._type)
+class ParametersConverter(object):
 
-    _props = ArrayOfProperty()
-    properties = obj.getProperties()
+    @staticmethod
+    def toJSON(params):
+        json = {'items': [ParameterConverter.toJSON(p) for p in params]}
+        return json
 
-    items = []
-    for k,v in properties.items():
-        p = Property()
-        p._name = k
-        p._value = v
-        items.append(p)
-    _props._item = items
-    target._properties = _props
-    return target
+    @staticmethod
+    def toSOAP(json):
+        soap = ArrayOfWorkflowParameter()
+        soap._item = [ParameterConverter.toSOAP(j) for j in json['items']]
+        return soap
+
+class WorkflowConverter(object):
+
+    @staticmethod
+    def toJSON(wf):
+        json = {'id': wf.id,
+                'name': wf.name,
+                'description': wf.description,
+                'input': ParametersConverter.toJSON(wf.input),
+                'output': ParametersConverter.toJSON(wf.output),
+                'attributes': ParametersConverter.toJSON(wf.attributes)}
+        return json
+
+    @staticmethod
+    def toSOAP(json):
+        soap = Workflow()
+        soap._id = json['id']
+        soap._name = json['name']
+        soap._description = json['description']
+        soap._inParameters = ParametersConverter.toSOAP(json['input'])
+        soap._outParameters = ParametersConverter.toSOAP(json['output'])
+        soap._attributes = ParametersConverter.toSOAP(json['attributes'])
+        return soap
+
+class WorkflowTokenConverter(object):
+
+    @staticmethod
+    def toJSON(tok):
+        json = {'id': tok.id,
+                'title': tok.title,
+                'wf_id': tok.wf.id,
+                'cur_name': tok.cur_name,
+                'cur_state': tok.cur_state,
+                'state': tok.state,
+                'start': tok.start,
+                'end': tok.end,
+                'xml': tok.xml}
+        return json
+
+    @staticmethod
+    def toSOAP(json):
+        soap = WorkflowToken()
+        soap._id = json['id']
+        soap._title = json['title']
+        soap._workflowId = json['wf_id']
+        soap._currentItemName = json['cur_name']
+        soap._currentItemState = json['cur_state']
+        soap._globalState = json['state']
+        soap._startDate = "%s" % (json['start'])
+        soap._endDate = "%s" % (json['end'])
+        soap._xmlContent = json['xml']
+        return soap
+
+class PluginObjectConverter(object):
+
+    @staticmethod
+    def toJSON(obj):
+        obj_id = obj.obj_id
+        obj_type = "%s:%s" % (obj.plugin.name, obj.type)
+        json = {'type': obj_type,
+                'id': obj_id,
+                'uri': "dunes://service.dunes.ch/CustomSDKObject?id='%s'&dunesName='%s'" % (obj_id, obj_type),
+                'props': obj.getProperties()}
+        return json
+
+    @staticmethod
+    def toSOAP(json):
+        soap = FinderResult()
+        soap._type = json['type']
+        soap._id = json['id']
+        soap._dunesURI = json['uri']
+
+        _props = ArrayOfProperty()
+        properties = json['props']
+        items = []
+        for k,v in properties.items():
+            p = Property()
+            p._name = k
+            p._value = v
+            items.append(p)
+        _props._item = items
+
+        soap._properties = _props
+        return soap
 
 def convertQueryResult(items):
     target = QueryResult()
@@ -89,3 +141,16 @@ def convertQueryResult(items):
     _res._item = items
     target._elements = _res
     return target
+
+def convertWorkflowTokenResult(tok):
+    res = []
+    outputs = tok.wf.output
+    results = tok.getResults()
+    for out in outputs:
+        attr = WorkflowTokenAttribute()
+        out = db.get(out)
+        attr._name = out.name
+        attr._type = out.type
+        attr._value = results.get(attr._name, "")
+        res.append(attr)
+    return res
--- vco/data4.py
+++ vco/data4.py
@@ -11,10 +11,21 @@ from datetime import datetime
 
 class _Item(object):
     _model = None
+    _converter = None
+
+    def __init__(self, model):
+        self._json = self._converter.toJSON(model)
+
+    def _asTarget(self):
+        return self._converter.toSOAP(self._json)
+
+    @classmethod
+    def findAllUri(cls):
+        return "data4://all/%s" % (cls.__name__)
 
     @classmethod
     def findAll(cls):
-        uri = "data4://all/%s" % (cls.__name__)
+        uri = cls.findAllUri()
         elems = memcache.get(uri)
         if elems is None:
             if issubclass(cls._model, models.TimedItem):
@@ -27,66 +38,74 @@ class _Item(object):
                 ttl = elems[0].ttl()
             else:
                 ttl = 0
-            #memcache.add(uri, elems, ttl)
 
-        items = [cls(i) for i in elems]
+            elems = [cls(i) for i in elems]
+            memcache.add(uri, elems, ttl)
+
+        items = [i._asTarget() for i in elems]
         return items
 
 class _IdItem(_Item):
 
     @classmethod
+    def findByIdUri(cls, id):
+        return "data4://id/%s/%s" % (cls.__name__, id)
+
+    @classmethod
     def findById(cls, id):
-        uri = "data4://id/%s/%s" % (cls.__name__, id)
+        uri = cls.findByIdUri(id)
         item = memcache.get(uri)
 
         if item is None:
             if issubclass(cls._model, models.TimedItem):
                 item = cls._model.getItem(id)
                 ttl = item.ttl()
-                #memcache.add(uri, item, ttl)
+                item = cls(item)
+                memcache.add(uri, item, ttl)
             else:
                 query = cls._model.all()
                 query.filter('id =', id)
                 item = query.get()
-                #memcache.add(uri, item)
+                item = cls(item)
+                memcache.add(uri, item)
 
-        if item is not None:
-            item = cls(item)
+        item = item._asTarget()
         return item
 
-class Plugin(types.ModuleInfo, _Item):
+class Plugin(_Item):
     _model = models.Plugin
+    _converter = convert.PluginConverter
 
     def __init__(self, model):
-        types.ModuleInfo.__init__(self)
-        self.__model = model
-
-        convert.convertPlugin(model, target=self)
+        _Item.__init__(self, model)
 
 class Workflow(types.Workflow, _IdItem):
     _model = models.Workflow
+    _converter = convert.WorkflowConverter
+
+    @classmethod
+    def findByNameUri(cls, name):
+        return "data4://id/%s/%s" % (cls.__name__, name)
 
     @classmethod
     def findByName(cls, name):
-        uri = "data4://id/%s/%s" % (cls.__name__, id)
+        uri = cls.findByNameUri(name)
         elems = memcache.get(uri)
         if elems is None:
             query = cls._model.all()
             query.filter('name =', name)
             elems = [x for x in query]
-            #memcache.add(uri, elems)
+            elems = [cls(i) for i in elems]
+            memcache.add(uri, elems)
 
-        items = [cls(i) for i in elems]
+        items = [e._asTarget() for e in elems]
         return items
 
     def __init__(self, model):
-        types.Workflow.__init__(self)
-        self.__model = model
-
-        convert.convertWorkflow(model, target=self)
+        _IdItem.__init__(self, model)
 
     def run(self, inputs):
-        wf = self.__model
+        wf = self.findById(self._id)
         tk_id = str(_uuid())
         token = models.WorkflowToken(id=tk_id,
                                      wf=wf)
@@ -96,20 +115,18 @@ class Workflow(types.Workflow, _IdItem):
         wf.initTokens(token, inputs)
         return WorkflowToken(token)
 
-class WorkflowToken(types.WorkflowToken, _IdItem):
+class WorkflowToken(_IdItem):
     _model = models.WorkflowToken
+    _converter = convert.WorkflowTokenConverter
 
     def __init__(self, model):
-        types.WorkflowToken.__init__(self)
-        self.__model = model
-
-        convert.convertWorkflowToken(model, target=self)
+        _IdItem.__init__(self, model)
 
     def cancel(self):
-        self.__model.cancel().put()
+        self.findById(self._id).cancel().put()
 
     def answer(self, inputs):
-        token = self.__model
+        token = self.findById(self._id)
         if token.state == models.WorkflowToken._WAITING:
             wf = token.wf
 
@@ -117,24 +134,27 @@ class WorkflowToken(types.WorkflowToken, _IdItem):
             wf.updateTokens(token, inputs)
 
     def result(self):
-        return convert.convertWorkflowTokenResult(self.__model)
+        return convert.convertWorkflowTokenResult(self.findById(self._id))
 
-class FinderResult(types.FinderResult, _IdItem):
+class FinderResult(_IdItem):
     _model = models.PluginObject
+    _converter = convert.PluginObjectConverter
 
     def __init__(self, model):
-        types.FinderResult.__init__(self)
-        self.__model = model
+        _IdItem.__init__(self, model)
 
-        convert.convertPluginObject(model, target=self)
+    @classmethod
+    def findUri(cls, type, id, query):
+        return "data4://finder/%s/%s/%s" % (type, id, query)
 
     @classmethod
     def find(cls, type, id=None, query=None, _query_result=False):
-        uri = "data4://finder/%s/%s/%s" % (type, id, query)
-        plugin, type = type.split(':')
-        plugin = models.Plugin.all().filter('name =', plugin).get()
+        uri = cls.findUri(type, id, query)
         elems = memcache.get(uri)
         if elems is None:
+            plugin, type = type.split(':')
+            plugin = models.Plugin.all().filter('name =', plugin).get()
+
             query = cls._model.allValid()
             query.filter('plugin =', plugin)
             query.filter('type =', type)
@@ -147,9 +167,10 @@ class FinderResult(types.FinderResult, _IdItem):
                 ttl = elems[0].ttl()
             else:
                 ttl = 0
-            #memcache.add(uri, elems, ttl)
+            elems = [cls(e) for e in elems]
+            memcache.add(uri, elems, ttl)
 
-        items = [cls(i) for i in elems]
+        items = [e._asTarget() for e in elems]
         if _query_result:
             return convert.convertQueryResult(items)
         else:
